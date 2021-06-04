ac3ae5d8370a6b4314eeb555d74dac2893ef2ec6
박서현 <shp7408@naver.com>
2021-06-04 23:11:09 +0900

    [MOD] 오름차순으로 배열 정리

--- main.py
+++ main.py
@@ -6,32 +6,14 @@ def solution(a, b, board):
     #print(board)
 
 
-    # 리스트 a의 값을 2진수로 바꿔서, a 에 저장하기
-    d = int(a[0]) + int(b[0])
-    print(bin(d)[2:])
-
-    e = int(a[1]) + int(b[1])
-    print(bin(e)[2:])
-
-    f = int(a[2]) + int(b[2])
-    print(bin(f)[2:])
-
-    g = int(a[3]) + int(b[3])
-    print(bin(g)[2:])
-
-    h = int(a[4]) + int(b[4])
-    print(bin(h)[2:])
-
-    i = int(a[5]) + int(b[5])
-    print(bin(i)[2:])
-
-    j = int(a[6]) + int(b[6])
-    print(bin(j)[2:])
-
-    k = int(a[7]) + int(b[7])
-    print(bin(k)[2:])
 
 
+    # 오름차순으로 정렬해서 연산 해보기
+    a.sort()
+    a.reverse()
+    b.sort()
+    #[39, 135, 145, 159, 161, 164, 182, 224]
+    #[1, 3, 25, 129, 160, 165, 228, 241]
 
     print(a)
     print(b)
