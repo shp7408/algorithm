663e3df768ec59bc2a414f066e1f413878bb7652
박서현 <62336151+shp7408@users.noreply.github.com>
2021-06-13 20:38:13 +0900

    [MOD] 2021.06.13 일 알고리즘 공부 시작 8일차

--- sw_expert_2056.py
+++ sw_expert_2056.py
@@ -15,20 +15,24 @@ for i in range(1, t + 1):
         print("#" + str(i), "-1")
 
     elif month == 2:
-        print(("#" + str(i) + " " + str(year) + "/" + str(month) + "/" + str(day)) if day <= 28 else (
+        print(("#" + str(i) + " " + str(year).zfill(4) + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)) if day <= 28 else (
                     "#" + str(i) + " -1"))
 
     elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-        print(("#" + str(i) + " " + str(year) + "/" + str(month) + "/" + str(day)) if day <= 31 else (
+        print(("#" + str(i) + " " + str(year).zfill(4) + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)) if day <= 31 else (
                     "#" + str(i) + " -1"))
 
     elif month == 4 or month == 6 or month == 9 or month == 11:
-        print(("#" + str(i) + " " + str(year) + "/" + str(month) + "/" + str(day)) if day <= 30 else (
+        print(("#" + str(i) + " " + str(year).zfill(4) + "/" + str(month).zfill(2) + "/" + str(day).zfill(2)) if day <= 30 else (
                     "#" + str(i) + " -1"))
 
 
 # 쉽지 않구만...
-#
+# 1. 요구사항 분석 : 문제 / 제약조건 파악
+# 2. 설계 : 연필로 직접 적어보며, 작은 test case를 해결해보기
+# 3. 구현 : 코딩 시작
+# 4. 테스트
+# 5. 유지 보수
 
 #1 2222/2/28 -1
 #2 -1
