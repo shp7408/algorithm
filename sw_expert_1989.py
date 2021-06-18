ad3219fe99abf28d0986f9853742fe63b5c04520
박서현 <62336151+shp7408@users.noreply.github.com>
2021-06-19 00:00:30 +0900

    [ADD] 2021.06.19 금 알고리즘 공부 시작 13일차

---
+++ sw_expert_1989.py
@@ -0,0 +1,18 @@
+# 1989. 초심자의 회문 검사
+
+a = int(input())
+list_str = []
+
+for i in range(1, a + 1):
+    list_str = input().split()
+    # print(list_str)
+    # print("#"+str(i))
+
+    odd_result = 0  # 초기화 시켜야 하는 부분 체크해야 함
+
+    for j in list_str:
+        if int(j) % 2 != 0:  # 2로 나누었을 때, 나누어 떨어지지 않으면,
+            odd_result += int(j)
+            # print(int(j))
+
+    print("#" + str(i), odd_result)
