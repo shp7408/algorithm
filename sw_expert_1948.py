4f9e5949f95e33885dca06ab65a9a55c99c0556a
박서현 <62336151+shp7408@users.noreply.github.com>
2021-06-23 23:21:59 +0900

    [MOD] 2021.06.23 수 알고리즘 공부 시작 15일차 코드 개선

--- sw_expert_1948.py
+++ sw_expert_1948.py
@@ -15,23 +15,13 @@ def sec_first_minus (first_Mon, fist_date, second_Mon, second_date) :
         return second_date - fist_date + 1
 
     else :
-
-        firstDate = 0
         firstDate = last_Date(first_Mon) - fist_date  + 1
-        #print("firstDate ", firstDate)
 
         middleDate = 0
         for month in range (first_Mon + 1 , second_Mon) :
             middleDate += last_Date(month)
-            #print(month, last_Date(month))
 
-        #print("middleDate ",middleDate)
-
-
-        lastDate = 0
         lastDate = second_date
-        #print("lastDate ",lastDate)
-
 
         return firstDate + middleDate + lastDate
 
@@ -42,5 +32,40 @@ for i in range (1, t+1) :
 
     # firstAndSecondList 리스트를 int 형으로 변환
     line_int = list(map(int, firstAndSecondList))
+    # 3 1 3 31
 
     print("#"+str(i), sec_first_minus(line_int[0], line_int[1], line_int[2], line_int[3] ))
+
+
+
+
+
+
+
+
+# 개선 코드 1
+T = int(input())
+Month_info = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def cntDay(m, d):
+    for i in range(0, m - 1):
+        d += Month_info[i]
+    return d
+
+for case in range(1, T + 1):
+    m1, d1, m2, d2 = map(int, input().split())
+    result = cntDay(m2, d2) - cntDay(m1, d1) + 1
+    print(f'#{case} {result}')
+
+
+
+# 개선 코드 2
+for case in range(1, T+1):
+    m1, d1, m2, d2 = map(int, input().split())
+    d1 += sum(Month_info[:m1])
+    d2 += sum(Month_info[:m2])
+    result = d2 - d1 + 1
+    print(f'#{case} {result}')
+
+
+
