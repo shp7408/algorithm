6adf0385030f9f9e405241b2e9b36e320f7a13a8
박서현 <62336151+shp7408@users.noreply.github.com>
2021-06-23 23:23:52 +0900

    [ADD] 2021.06.23 수 알고리즘 공부 시작 15일차

--- programmers_포켓몬.py
+++ programmers_포켓몬.py
@@ -1,10 +1,6 @@
 # 프로그래머스 / 레벨 1 / 포켓몬
 
 def solution(nums):
-    answer = 0
-
     numSet = set(nums)
-
     answer = len(numSet) if len(numSet) < len(nums) / 2 else len(nums) / 2
-
     return answer
