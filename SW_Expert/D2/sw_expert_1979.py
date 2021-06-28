2ab1e082e027f04787394daf64ac9b1cb1607758
박서현 <62336151+shp7408@users.noreply.github.com>
2021-06-28 23:54:21 +0900

    [MOD] 2021.06.28 월 코드 수정

--- SW_Expert/D2/sw_expert_1979.py
+++ SW_Expert/D2/sw_expert_1979.py
@@ -8,7 +8,13 @@
 # 5. 열 중에 01110 문자열 체크
 # 6. 체크해서 있을 때 마다, result+1
 
-
+# 1. 한 줄 한 줄을 문자열 그대로 받는다.
+# 2. 문자열의 공백을 제거한다.
+# 3. 문자열을 리스트화 한다.
+# 4. 문자열의 맨 앞, 맨 마지막을 0으로 감싼다.
+# 5. 리스트화 된 문자열의 0 마지막 인덱스를 0*N 으로 감싼다.
+# 6. 01110 체크
+# 7.
 
 T = int(input())
 
@@ -16,32 +22,20 @@ for case in range(1, T + 1):
     N, K = map(int, input().split())
 
     aList = []
-    zeroList = []
-    twoTwoList = []
 
     for i in range(N):
-        zeroList.append('0')
+        a = input()
+        a1 = a.replace(" ", "")
+        aList.append(a1)
 
-    #print(zeroList)
+    firstLast = '0'*N #00000
+    aList.append(firstLast)
+    aList.insert(0, firstLast)
 
-    for i in range(N):
-        aList = input().split()
-        twoTwoList.append(aList)
-
-
-    twoTwoList.insert(0, zeroList)
-    twoTwoList.append(zeroList)
-    print(case, twoTwoList[N+1])
-
-    for i in range(N+2):
-        twoTwoList[i].append('0')
-    print(case, twoTwoList[0])
-
-
-
-
-
-
+    bList =[]
+    for i in aList:
+        i2 = "0" + i + "0"
+        bList.append(i2)
 
 
 
