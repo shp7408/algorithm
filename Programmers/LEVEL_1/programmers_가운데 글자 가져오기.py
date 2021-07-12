85e8a3b55c383c1a2632852324ac5b897ac96c41
박서현 <62336151+shp7408@users.noreply.github.com>
2021-07-12 23:58:17 +0900

    [ADD] 2021.07.12 수 알고리즘 공부 시작 18일차

--- Programmers/LEVEL_1/programmers_가운데 글자 가져오기.py
+++ Programmers/LEVEL_1/programmers_가운데 글자 가져오기.py
@@ -1,12 +1,4 @@
 # 프로그래머스 / 레벨 1 / 가운데 글자 가져오기
 
 def solution(s):
-    answer = ''
-
-    a = int((len(s) - 1) / 2)
-
-    print(type(a))
-
-    print(s[int(len(s) / 2 - 1): int(len(s) / 2 + 2)] if len(s) % 2 == 0
-          else s[int(len(s) - 1 / 2)])
-
+    return s[int(len(s) / 2 - 1): int(len(s) / 2 + 1)] if len(s) % 2 == 0 else s[int((len(s) - 1) / 2)]
