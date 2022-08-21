dc57c00fe1d9adf83d87fd5a963b654da82e2458
shp7408 <62336151+shp7408@users.noreply.github.com>
2022-08-21 21:23:19 +0900

    프로그래머스 문제를 IDE에서 풀 수 있도록 하는 코드

---
+++ Java/Programmers/Main.java
@@ -0,0 +1,34 @@
+// Programmers Java 문제 풀이 시,
+// IDE에서 코드 작성 및 test code 돌릴 수 있는 템플릿
+
+/**
+ * Solution
+ * Programmers에서 제출할 코드
+ * @param args
+ * 출제 문제에 따라 param의 수, 타입이 달라질 수 있다.
+ * 그 때, 그 때, 맞게 변경해서 사용하자.
+ */
+class Solution {
+    public int sol(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+
+    Solution() {}
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+//        test code
+        Solution solution = new Solution();
+        System.out.println("============= 1 번");
+        int case1 = solution.sol(15);
+        if (case1 == 6) {
+            System.out.println("passsssss!!!! : " + case1);
+        } else {
+            System.out.println("false!" + case1);
+        }
+    }
+}
